d781b684e1f62f6440e8a85f8ee72e548610d35d
Eliza4021 <wangarielizabeth4021@gmail.com>
2023-02-22 01:24:07 -0800

    added numberz file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers.
+ *
+ * Return:Always 0(success)
+ */
+
+int main(void)
+{
+	 unsigned char f[10] = "0123456789";
+	int i;
+
+	for (i = 0; i < 0; i++)
+	{
+		putchar(f[i]);
+	}
+	putchar('\n');
+	return (0);
+}
