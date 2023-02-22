cccd27a8800b5176a095c2c90e280169c16ff154
Eliza4021 <wangarielizabeth4021@gmail.com>
2023-02-22 02:06:34 -0800

    added hexadecimal file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the base sixteen numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s[16] = "0123456789abcdef";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+	return (0);
+}
