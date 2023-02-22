9c527b4b4173c130de036b19e3ea50927d9a08f2
Eliza4021 <wangarielizabeth4021@gmail.com>
2023-02-22 00:33:30 -0800

    updated alphabets file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char d[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ";
+	char d[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 52; i++)
